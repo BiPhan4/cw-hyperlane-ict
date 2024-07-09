139e36dcb6a64eaa3cff9dab90db6c38748d32aa
BiPhan4 <biphan374@gmail.com>
2024-07-09 10:28:45 -0400

    boot local eth node

--- interchaintest/start_test.go
+++ interchaintest/start_test.go
@@ -6,12 +6,16 @@ import (
 	"testing"
 	"time"
 
+	"cosmossdk.io/math"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
 
 	"github.com/strangelove-ventures/interchaintest/v7"
 	"github.com/strangelove-ventures/interchaintest/v7/chain/ethereum"
 	"github.com/strangelove-ventures/interchaintest/v7/testreporter"
 )
+
+// NOTE: Foundry comes with pre loaded accounts, and private keys with seed phrases
 
 func TestEthereum(t *testing.T) {
 
@@ -33,10 +37,41 @@ func TestEthereum(t *testing.T) {
 	ctx := context.Background()
 
 	// Get default ethereum chain config for anvil
-	anvilConfig := ethereum.DefaultEthereumAnvilChainConfig("ethereum")
+	anvilConfig := LocalEthereumAnvilChainConfig("ethereum")
 
-	fmt.Println(f)
-	fmt.Println(client)
-	fmt.Println(network)
+	cf := interchaintest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*interchaintest.ChainSpec{
+		{
+			ChainName:   "ethereum",
+			Name:        "ethereum",
+			Version:     "latest",
+			ChainConfig: anvilConfig,
+		},
+	})
+
+	chains, err := cf.Chains(t.Name())
+	require.NoError(t, err)
+
+	ethereumChain := chains[0].(*ethereum.EthereumChain)
+
+	ic := interchaintest.NewInterchain().
+		AddChain(ethereumChain)
+
+	require.NoError(t, ic.Build(ctx, eRep, interchaintest.InterchainBuildOptions{
+		TestName:  t.Name(),
+		Client:    client,
+		NetworkID: network,
+		// BlockDatabaseFile: interchaintest.DefaultBlockDatabaseFilepath(),
+		SkipPathCreation: true, // Skip path creation, so we can have granular control over the process
+	}))
+	fmt.Println("Interchain built")
+
+	// Create and fund a user using GetAndFundTestUsers
+	ethUserInitialAmount := math.NewInt(2 * ethereum.ETHER)
+	users := interchaintest.GetAndFundTestUsers(t, ctx, "user", ethUserInitialAmount, ethereumChain)
+	// ethUser := users[0]
+	fmt.Println(users)
+
+	// Sleep for additional testing
+	time.Sleep(10 * time.Hour)
 
 }
